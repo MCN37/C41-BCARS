b9c417fcf0292a4f452522a697aed7ad56c70aa9
MCM37 <84409804+MCN37@users.noreply.github.com>
2021-09-27 15:06:47 -0500

    Update Player.js

--- C41 base C40/MovimientoDePelotaAsincorono/js/Player.js
+++ C41 base C40/MovimientoDePelotaAsincorono/js/Player.js
@@ -4,7 +4,7 @@ class Player {
      this.index = null;
      this.distance = 0;
      this.name = null;
-     this.rank = null;
+
 
     }
   
@@ -38,18 +38,7 @@ class Player {
     }
 
 
-    getCarsAtEnd(){
-
-      database.ref('CarsAtEnd').on('value',(data)=>{
-        this.rank = data.val();
-      })
-    }
-   
-   static updateCarsAtEnd(rank){
-     database.ref('/').update({
-       CarsAtEnd: rank
-     })
-   }
+    
 
    
   }
